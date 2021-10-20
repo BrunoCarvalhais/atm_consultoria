1441750396c74bc82f54b639728b6fb6fc9f3dd9
BrunoCarvalhais <carvalhaisazevedo@gmail.com>
2021-10-20 13:15:23 -0300

    commit2

---
+++ lib/TelaCliente.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TelaCliente extends StatefulWidget {
+  const TelaCliente({Key? key}) : super(key: key);
+
+  @override
+  _TelaClienteState createState() => _TelaClienteState();
+}
+
+class _TelaClienteState extends State<TelaCliente> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/TelaEmpresa.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TelaEmpresa extends StatefulWidget {
+  const TelaEmpresa({Key? key}) : super(key: key);
+
+  @override
+  _TelaEmpresaState createState() => _TelaEmpresaState();
+}
+
+class _TelaEmpresaState extends State<TelaEmpresa> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
